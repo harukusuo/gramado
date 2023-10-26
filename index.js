301e4347d7d0ae394d1f7b9fbb8e99b8d64c94af
sah <128996530+harukusuo@users.noreply.github.com>
2023-10-26 15:02:23 -0300

    upload de arquivos

---
+++ index.js
@@ -0,0 +1,10 @@
+  //menu sanduiche
+
+  function clickMenu() {
+    var itens = document.getElementById('itens');
+    if (itens.style.display == 'block') {
+      itens.style.display = 'none';
+    } else {
+      itens.style.display = 'block';
+    }
+  }
